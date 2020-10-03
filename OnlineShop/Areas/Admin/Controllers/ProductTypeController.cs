3089727defd1ad8a0afe8e3c49d027af855f1f33
Mirza Adnan <jmirza7@outlook.com>
2020-10-04 04:02:24 +0600

    Added all functionality of ProductType also added partial view for action button and alertify msg

--- OnlineShop/Areas/Admin/Controllers/ProductTypeController.cs
+++ OnlineShop/Areas/Admin/Controllers/ProductTypeController.cs
@@ -23,13 +23,13 @@ namespace OnlineShop.Areas.Admin.Controllers
             return View(_dbContext.productTypes.ToList());
         }
 
-        // Create Get Action Method
+        // Get Create Action Method
         public IActionResult Create()
         {
             return View();
         }
 
-        // Create Post Action Method
+        // Post Create Action Method
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create(ProductType productType)
@@ -37,6 +37,110 @@ namespace OnlineShop.Areas.Admin.Controllers
             if (ModelState.IsValid)
             {
                 _dbContext.productTypes.Add(productType);
+                await _dbContext.SaveChangesAsync();
+                TempData["Save"] = "Data Saved Successfully";
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(productType);
+        }
+
+        // Get Edit Action Method
+        public IActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var productType = _dbContext.productTypes.Find(id);
+            if (productType == null)
+            {
+                return NotFound();
+            }
+
+            return View(productType);
+        }
+        // Post Edit Action Method
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(ProductType productType)
+        {
+            if (ModelState.IsValid)
+            {
+                _dbContext.Update(productType);
+                await _dbContext.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(productType);
+        }
+
+        // Get Details Action Method
+        public IActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var productType = _dbContext.productTypes.Find(id);
+            if (productType == null)
+            {
+                return NotFound();
+            }
+
+            return View(productType);
+        }
+        // Post Details Action Method
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Details(ProductType productType)
+        {
+            return RedirectToAction(nameof(Index));
+        }
+
+
+        // Get Delete Action Method
+        public IActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var productType = _dbContext.productTypes.Find(id);
+            if (productType == null)
+            {
+                return NotFound();
+            }
+
+            return View(productType);
+        }
+        // Post Delete Action Method
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Delete(int? id, ProductType productType)
+        {
+            if (id==null)
+            {
+                return NotFound();
+            }
+
+            if (id != productType.ID)
+            {
+                return NotFound();
+            }
+            var productTyp = _dbContext.productTypes.Find(id);
+
+            if (productTyp==null)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                _dbContext.Remove(productTyp);
                 await _dbContext.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
             }
